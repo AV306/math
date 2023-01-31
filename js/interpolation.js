bd1773381c1c2b1cd433f31bdf562540c9c2cb26
AV3_08 <66512356+AV306@users.noreply.github.com>
2023-01-31 12:29:06 +0800

    Update interpolation.js

--- js/interpolation.js
+++ js/interpolation.js
@@ -13,7 +13,7 @@ function lerp( min, max, val )
 function smoothstep( min, max, val )
 {
   var d = max - min;
+  var x = val * val * (3 - 2*val);
   
-  
-  //return clamp(
+  return clamp( min, max, min + (d * val);
 }
