93baf5c8db4582afb1e81e44abc0275b2606e516
AV3_08 <66512356+AV306@users.noreply.github.com>
2023-02-03 11:50:20 +0800

    Update util.js

--- js/util.js
+++ js/util.js
@@ -11,3 +11,13 @@ function clamp( v, min, max )
 {
   return Math.min( Math.max( v, min), max );
 }
+
+function mapRange( vmin, vmax, tmin, tmax, val )
+{
+  var vrange = vmax - vmin;
+  var trange = tmax - tmin;
+  
+  val = clamp( vmin, vmax, val );
+  
+  return clamp( tmin, tmax, trange * (val / vrange) );
+}
